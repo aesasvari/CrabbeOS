c5c754dc469eb4b595de66f075ac901be5f1328e
aesasvari <aesasvari@gmail.com>
2021-06-08 17:37:12 +0100

    Just some small changes. Added null section for the BIOS parameters.

--- boot.asm
+++ boot.asm
@@ -1,7 +1,27 @@
-ORG 0x7c00
+ORG 0
 BITS 16
 
+_start:
+    jmp short start
+    nop
+
+times 33 db 0
+
 start:
+    jmp 0x7C0:next
+
+next:
+    cli
+    mov ax, 0x7C0
+    mov ds, ax
+    mov es, ax
+
+    mov ax, 0x00
+    mov ss, ax
+    mov sp, 0x7C00
+
+    sti
+
     mov si, message
     call print
     jmp $ 
