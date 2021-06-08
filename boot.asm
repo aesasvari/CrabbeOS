23ee7c339ec7609d49c9567156789378bef3e425
aesasvari <aesasvari@gmail.com>
2021-06-08 18:52:25 +0100

    The message that prints to the screen upon running the binary can now be read from a file so long as it is below 512B. (To change the message, modify message.txt)

--- boot.asm
+++ boot.asm
@@ -10,19 +10,7 @@ times 33 db 0
 start:
     jmp 0x7C0:next
 
-zero_interrupt:
-    mov ah, 0eh
-    mov al, '0'
-    mov bx, 0x00
-    int 0x10
-    iret
 
-one_interrupt:
-    mov ah, 0eh
-    mov al, '1'
-    mov bx, 0x00
-    int 0x10
-    iret
 
 next:
     cli
@@ -36,17 +24,23 @@ next:
 
     sti
 
-    mov word[ss:0x00], zero_interrupt
-    mov word[ss:0x02], 0x7C0
+    mov ah, 2
+    mov al, 1
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
+    mov bx, buffer
+    int 0x13
 
-    int 0
+    jc error
 
-    mov word[ss:0x04], one_interrupt
-    mov word[ss:0x06], 0x7C0
+    mov si, buffer
+    call print
+    
+    jmp $
 
-    int 1
-
-    mov si, message
+error:
+    mov si, error_message
     call print
     jmp $ 
 
@@ -69,8 +63,10 @@ printc:
     int 0x10
     ret
 
-message:
-    db 'Hello World', 0
+error_message:
+    db 'Failed to load sector', 0
 
 times 510-($-$$) db 0
 dw 0xAA55
+
+buffer:
