d352c9248761d65fbc95b4cb2c7943cd0f793301
aesasvari <aesasvari@gmail.com>
2021-06-24 23:17:39 +0100

    Added text support. Prints Hello World to the screen.

--- src/kernel.c
+++ src/kernel.c
@@ -1,6 +1,76 @@
 #include "kernel.h"
+#include <stddef.h>
+#include <stdint.h>
+
+uint16_t* video_mem = 0;
+uint16_t terminal_row = 0;
+uint16_t terminal_column = 0;
+
+uint16_t terminal_make_char(char c, char colour)
+{
+    return (colour << 8) | c;
+}
+
+void terminal_put_char(int x, int y, char c, char colour)
+{
+    video_mem[(y * VGA_WIDTH) + x] = terminal_make_char(c, colour);
+}
+
+void terminal_write_char(char c, char colour)
+{
+    if (c == '\n')
+    {
+        terminal_column = 0;
+        terminal_row++;
+        return;
+    }
+
+    terminal_put_char(terminal_column, terminal_row, c, colour);
+    terminal_column++;
+    if (terminal_column >= VGA_WIDTH)
+    {
+        terminal_column = 0;
+        terminal_row++;
+    }
+
+}
+
+void terminal_init()
+{
+    video_mem = (uint16_t*)(0xB8000);
+    terminal_row = 0;
+    terminal_column = 0;
+
+    for (int y = 0; y < VGA_HEIGHT; y++)
+    {
+        for (int x = 0; x < VGA_WIDTH; x++)
+        {
+            terminal_put_char(x, y, ' ', 0);
+        }
+    }
+}
+
+size_t strlen(const char* str)
+{
+    size_t len = 0;
+    while(str[len])
+    {
+        len++;
+    }
+    return len;
+}
+
+void print(const char* str)
+{
+    size_t len = strlen(str);
+    for (int i = 0; i < len; i++)
+    {
+        terminal_write_char(str[i], 15);
+    }
+}
 
 void kernel_main()
 {
-
+    terminal_init();
+    print("Hello World!\nTest");
 }
