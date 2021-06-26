5366ef458675ed12d4df1468c406c5cdd7df2dfe
aesasvari <aesasvari@gmail.com>
2021-06-26 04:27:52 +0100

    Added filesystem parser.

--- src/kernel.c
+++ src/kernel.c
@@ -6,6 +6,8 @@
 #include "memory/heap/kheap.h"
 #include "memory/paging/paging.h"
 #include "disk/disk.h"
+#include "fs/pparser.h"
+#include "string/string.h"
 
 uint16_t* video_mem = 0;
 uint16_t terminal_row = 0;
@@ -56,16 +58,6 @@ void terminal_init()
     }
 }
 
-size_t strlen(const char* str)
-{
-    size_t len = 0;
-    while(str[len])
-    {
-        len++;
-    }
-    return len;
-}
-
 void print(const char* str)
 {
     size_t len = strlen(str);
@@ -76,6 +68,7 @@ void print(const char* str)
 }
 
 static struct paging_4gb_chunk* kernel_chunk = 0;
+
 void kernel_main()
 {
     terminal_init();
@@ -92,4 +85,8 @@ void kernel_main()
     enable_paging();
 
     enable_interrupts();
+
+    struct path_root* root_path = pathparser_parse("0:/bin/bash", NULL);
+    if (root_path);
+
 }
